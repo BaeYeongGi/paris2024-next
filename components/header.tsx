d9f008189478e1d4ef7cc99ef83a0476c20612aa
BaeYeongGi <byg5913@gmail.com>
2025-05-12 15:58:29 +0900

    [feat]: header.module.css -> tailwind 변경

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Link from 'next/link';
-import styles from '@/styles/header.module.css';
 import Image, { StaticImageData } from 'next/image';
 import { usePathname } from 'next/navigation';
 import iconNateLogo from '@/public/images/icon_nate_logo.png';
@@ -60,27 +59,28 @@ const Header = ({total, medal}: headerProps) => {
   }
 
   return (
-    <header className={path.includes('poll') ? `${styles.header} ${styles.poll}` : styles.header}>
-      <Image className={styles.header_bg} src={getHeaderBackground(path.split('/')[1])} alt="" width="161" height="112" />
-      <div className={styles.header_top_area}>
-        <ul className={styles.header_nav}>
+    <header className={`w-full h-28 px-5 pt-4.5 box-border relative ${path.includes('poll') ?
+    'bg-linear-90 bg-gradient-to-r from-[#304a97] from-10% to-[#ecbbc6] to-100%' : 'bg-linear-90 bg-gradient-to-r from-[#278d8f] from-10% to-[#efbcc7] to-100%'}`}>
+      <Image className='absolute top-0 right-0' src={getHeaderBackground(path.split('/')[1])} alt="" width="161" height="112" />
+      <div className='w-full flex items-center justify-between mb-2 relative z-10'>
+        <ul className='flex items-center space-x-2 text-[#ffffff] tracking-[-0.5px] text-[16px]'>
           <li>
             <Link href="https://m.nate.com" aria-label="네이트">
-              <Image className={styles.logo} src={iconNateLogo} alt='네이트' width="22" height="22"/>
+              <Image className='mr-0.5' src={iconNateLogo} alt='네이트' width="22" height="22"/>
             </Link>
           </li>
-          <li><Link href="https://m.news.nate.com/spo" className={styles.sports}>스포츠</Link></li>
+          <li><Link href="https://m.news.nate.com/spo" className='text-[22px] font-bold'>스포츠</Link></li>
           <li><Link href="https://m.news.nate.com/">뉴스</Link></li>
           <li><Link href="https://m.news.nate.com/ent">연예</Link></li>
         </ul>
-        <a href="https://m.news.nate.com/search" className={styles.search}>
+        <a href="https://m.news.nate.com/search">
           <Image src={iconHeaderSearch} alt="검색 페이지로 이동" width="22" height="22"/>
         </a>
       </div>
-      <div className={styles.header_bottom_area}>
-        <ul className={styles.title_wrap}>
+      <div className='flex justify-between items-center relative z-10'>
+        <ul className='flex items-end'>
           <li>
-            <Link href="/" className={styles.title}>
+            <Link href="/" className="mr-0.25">
               { path === "/" ?
               <h1><Image src={iconHeaderTitle} alt='봉쥬르 paris' width="102" height="52" /></h1>
               :
@@ -88,20 +88,20 @@ const Header = ({total, medal}: headerProps) => {
             </Link>
           </li>
           <li>
-            <button className={`${styles.share} ${styles.gnb_share}`} onClick={shareHandler}>
+            <button className='mb-1' onClick={shareHandler}>
               <Image src={iconHeaderShare} alt='공유하기' width="30" height="24"/>
             </button>
           </li>
         </ul>
-        <Link href="/medalist" className={styles.medal_wrap}>
-            <p className={styles.rank}><span>종합</span> {total}위</p>
-            <ul className={`${styles.medal_count}`}>
+        <Link href="/medalist" className='flex items-center relative'>
+            <p className='text-[14px] font-semibold tracking-[-0.8px] mr-2.5 text-[#ffffff] font-sans'><span>종합</span> {total}위</p>
+            <ul className='flex'>
             {
               medal.map((item: medalType) => {
                 return (
-                  <li key={item.name} className={styles[item.class]}>
-                    <Image src={item.img} alt={`${item.name} 획득 수`} width="35" height="35" />
-                    <span>{item.count}</span>
+                  <li key={item.name} className='text-center relative w-8.75 h8.75 flex items-center justify-center space-x-1'>
+                    <Image src={item.img} alt={`${item.name} 획득 수`} width="35" height="35" className='absolute' />
+                    <span className='block text-[#ffffff] font-bold z-1 indent-[-2px] font-sans text-shadow-sm'>{item.count}</span>
                   </li>
                 )
               })
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Arial', 'Malgun Gothic', 'Dotum', 'Helvetica', 'sans-serif'],
+      },
+    },
+  },
+};
