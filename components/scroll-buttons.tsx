1d40851384e2fecd8c8573c07cb4220dc67a5dd0
BaeYeongGi <byg5913@gmail.com>
2025-05-17 21:11:53 +0900

    [feat]: scroll-buttons.module.css -> tailwind

--- components/scroll-buttons.tsx
+++ components/scroll-buttons.tsx
@@ -1,7 +1,7 @@
 'use client';
 import IconScrollHome from '@/public/images/icon_scroll_home';
 import IconScrollTop from '@/public/images/icon_scroll_top';
-import styles from '@/styles/scroll-buttons.module.css';
+
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
@@ -22,12 +22,18 @@ const ScrollButtons = () => {
     }
   },[height])
 
+  const styles = {
+    wrap: 'fixed right-[15px] bottom-[-100px] rounded-[40px] border-b border-solid border-[#ededed] overflow-hidden z-10 shadow-[0_4px_4px_0_rgba(0,0,0,0.1)] transition-[bottom] duration-300',
+    wrapFixed: 'bottom-[15px]',
+    link: 'flex justify-center items-center w-[44px] h-[40px] bg-[#ffffff]',
+  }
+
   return (
-    <div className={height ? `${styles.scroll_btns_wrap} ${styles.fixed}` : styles.scroll_btns_wrap}>
-      <Link href="/" className={styles.home}>
+    <div className={`${styles.wrap} ${height ? styles.wrapFixed : ''}`}>
+      <Link href="/" className={`${styles.link} border-b border-solid border-[#ededed]`}>
         <IconScrollHome />
       </Link>
-      <Link href="#" className={styles.top}>
+      <Link href="#" className={`${styles.link} p-0`}>
         <IconScrollTop />
       </Link>
     </div>
